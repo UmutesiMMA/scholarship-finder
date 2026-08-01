0d46f311f4c156a2c7f2918a8cb618b40e8e084f
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-08-01 13:29:09 +0200

    refactor to use nodejs server to abstract api key

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/api/scholarships", async (req, res) => {
+  try {
+    const response = await axios.get(process.env.BASE_URL, {
+      params: req.query,
+      headers: {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": process.env.API_HOST,
+      },
+    });
+    res.json(response.data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
